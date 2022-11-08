58f9ea26f1fc391e548eba10b75c14295b2b4b72
PhamNgocPhat <114579161+phamphat261023@users.noreply.github.com>
2022-11-08 16:53:54 +0700

    CTDL2

--- QLDiemSinhVien.cpp
+++ QLDiemSinhVien.cpp
@@ -4,7 +4,9 @@ using namespace std;
 // Bang diem
 struct BangDiem
 {
-    double diem45p, diemGK, diemThi;
+    double diem45p;
+    double diemGK;
+    double diemThi;
     double diemTBHK = diem45p * 0.2 + diemGK * 0.3 + diemThi * 0.5;
 };
 
@@ -75,178 +77,35 @@ void insertTailBD(ListBangDiem &listBD, NodeBD *p)
 void NhapBD(BangDiem &bd)
 {
     cout << "Nhap diem 45': ";
-    fflush(stdin);
     cin >> bd.diem45p;
     cout << "\nNhap diem GK: ";
-    fflush(stdin);
     cin >> bd.diemGK;
     cout << "\nNhap diem thi: ";
-    fflush(stdin);
     cin >> bd.diemThi;
 }
 
 void InBD(BangDiem bd)
 {
-    cout << setw(8) << bd.diem45p << setw(12) << bd.diemGK << setw(16) << bd.diemThi << setw(8) << bd.diemTBHK << endl;
-}
-
-// Lop
-struct Lop
-{
-    int maLop;
-    string tenLop;
-};
-
-typedef Lop lop;
-
-struct NodeLop
-{
-    Lop infoLop;
-    struct NodeLop *next;
-};
-
-struct ListLop
-{
-    NodeLop *head;
-    NodeLop *tail;
-};
-
-ListLop listL;
-
-void initLop(ListLop &listL)
-{
-    listL.head = NULL;
-    listL.tail = NULL;
-}
-
-NodeLop *getNodeLop(Lop x)
-{
-    NodeLop *p;
-    p = new NodeLop;
-    if (p == NULL)
-    {
-        cout << "Khong du bo nho !!";
-        exit(1);
-    }
-    p->infoLop = x;
-    p->next = NULL;
-
-    return p;
-}
-
-void insertFirstLop(ListLop &listL, NodeLop *p)
-{
-    if (listL.head == NULL)
-    {
-        listL.head = p;
-        listL.tail = p;
-    }
-    else
-    {
-        p->next = listL.head;
-        listL.head = p;
-    }
-}
-
-void insertTailLop(ListLop &listL, NodeLop *p)
-{
-    if (listL.tail == NULL)
-    {
-        listL.head = p;
-        listL.tail = p;
-    }
-    else
-    {
-        p->next = listL.tail;
-        listL.tail = p;
-    }
-}
-
-void NhapLop()
-{
-}
-
-// GiaoVien
-
-struct GiaoVien
-{
-    int maGV;
-    string tenGV;
-};
-
-typedef GiaoVien gv;
-
-struct NodeGV
-{
-    GiaoVien infoGV;
-    struct NodeGV *next;
-};
-
-struct ListGiaoVien
-{
-    NodeGV *head;
-    NodeGV *tail;
-};
-
-ListGiaoVien listgv;
-
-void intitGV(ListGiaoVien &listgv)
-{
-    listgv.head = NULL;
-    listgv.tail = NULL;
-}
-
-NodeGV *getNodeGV(GiaoVien x)
-{
-    NodeGV *p;
-    p = new NodeGV;
-    if (p == NULL)
-    {
-        cout << "Khong du bo nho !!";
-        exit(1);
-    }
-    p->infoGV = x;
-    p->next = NULL;
-
-    return p;
-}
-
-void insertFirstGV(ListGiaoVien &listgv, NodeGV *p)
-{
-    if (listgv.head == NULL)
-    {
-        listgv.head = p;
-        listgv.tail = p;
-    }
-    else
-    {
-        p->next = listgv.head;
-        listgv.head = p;
-    }
-}
-
-void insertTailGV(ListGiaoVien &listgv, NodeGV *p)
-{
-    if (listgv.tail == NULL)
-    {
-        listgv.head = p;
-        listgv.tail = p;
-    }
-    else
-    {
-        p->next = listgv.tail;
-        listgv.tail = p;
-    }
+    cout << setw(10) << "Diem 45 phut"
+         << " | ";
+    cout << setw(10) << "Diem giua ki"
+         << " | ";
+    cout << setw(10) << "Diem thi"
+         << " | ";
+    cout << setw(10) << "Diem TB hoc ki"
+         << " | " << endl;
+    cout << setw(12) << bd.diem45p << " | ";
+    cout << setw(10) << bd.diemGK << " | ";
+    cout << setw(10) << bd.diemThi << " | ";
+    cout << setw(10) << bd.diemTBHK << " | " << endl;
 }
 
 // Sinh vien
 struct SinhVien
 {
     int maSV;
-    string tenSV, diaChi;
-    Lop lop[100];
-    GiaoVien gv[100];
-    BangDiem bd[200];
+    string tenSV, diaChi, giaoVien, lop;
+    BangDiem bd;
 };
 
 typedef SinhVien sv;
@@ -271,7 +130,7 @@ void initSV(ListSinhVien &listsv)
     listsv.tail = NULL;
 };
 
-NodeSV *getNodeGV(SinhVien x)
+NodeSV *getNodeSV(SinhVien x)
 {
     NodeSV *p;
     p = new NodeSV;
@@ -312,4 +171,80 @@ void insertTailSV(ListSinhVien &listsv, NodeSV *p)
         p->next = listsv.tail;
         listsv.tail = p;
     }
+}
+
+void NhapBDSinhVien(SinhVien &sv)
+{
+    cout << "Nhap ma sinh vien: ";
+    fflush(stdin);
+    cin >> sv.maSV;
+    cout << "\nNhap ten sinh vien: ";
+    fflush(stdin);
+    getline(cin, sv.tenSV);
+    cout << "\nNhap dia chi: ";
+    getline(cin, sv.diaChi);
+    cout << "\nNhap lop: ";
+    getline(cin, sv.lop);
+    cout << "\nNhap giao vien: ";
+    getline(cin, sv.giaoVien);
+    cout << "\n---------------------------------------\n";
+    cout << "\nNhap bang diem cua sinh vien:\n ";
+    NhapBD(sv.bd);
+}
+
+void InBDSinhVien(SinhVien sv)
+{
+    cout << "Danh sach: " << endl;
+    cout << setw(10) << "Ma sinh vien"
+         << " | ";
+    cout << setw(10) << "Ho ten"
+         << " | ";
+    cout << setw(10) << "Dia chi"
+         << " | ";
+    cout << setw(10) << "Lop"
+         << " | ";
+    cout << setw(10) << "Giao viem"
+         << " | " << endl;
+    cout << setw(12) << sv.maSV << " | ";
+    cout << setw(10) << sv.tenSV << " | ";
+    cout << setw(10) << sv.diaChi << " | ";
+    cout << setw(10) << sv.lop << " | ";
+    cout << setw(10) << sv.giaoVien << " | " << endl;
+    cout << "Bang diem" << endl;
+    InBD(sv.bd);
+}
+
+void NhapDSBD(ListSinhVien &listsv)
+{
+    int n;
+    cout << "Nhap so luong bang diem: ";
+    initSV(listsv);
+    cin >> n;
+    cin.ignore();
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\nNhap bang diem thu " << i + 1 << endl;
+        SinhVien sv;
+        NhapBDSinhVien(sv);
+        NodeSV *p = getNodeSV(sv);
+        insertTailSV(listsv, p);
+    }
+}
+
+void InDSBD(ListSinhVien listsv)
+{
+    cout << "---------------------------- DANH SACH BANG DIEM CUA HOC SINH ----------------------------" << endl;
+    for (NodeSV *p = listsv.head; p != NULL; p = p->next)
+    {
+        InBDSinhVien(p->infoSV);
+        cout << "\n__________________________________________________________________________________________\n";
+    }
+}
+
+int main()
+{
+    ListSinhVien listsv;
+    NhapDSBD(listsv);
+    InDSBD(listsv);
+    return 0;
 }
